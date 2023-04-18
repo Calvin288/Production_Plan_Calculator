f38f115e3bd4f9c364800f0da7e26b4aea7f4101
Nidhi153 <npshahaa@gmail.com>
2023-04-18 17:31:09 +0800

    Javadoc changes and patched some bugs, added some alerts

--- src/main/java/src/main/java/AppMain.java
+++ src/main/java/src/main/java/AppMain.java
@@ -2,6 +2,19 @@ package src.main.java;
 
 import javafx.application.Application;
 
+/**
+ * <h1>PPC1 Calculator</h1>
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ * <p>
+ * Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ *
+ * @author  Nidhi Shah, Albertus Alexander and Calvin Wiogo
+ * @version 1.0
+ * @since   2023-04-01
+ */
 public class AppMain {
     public static void main(String[] args) {
         Application.launch(Main.class,args);
---
+++ src/test/java/src/main/java/functionCControllerTest.java
@@ -0,0 +1,15 @@
+package src.main.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class functionCControllerTest {
+
+    @Test
+    void toclick() {
+        //normal case
+
+    }
+}
