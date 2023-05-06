f01d0615eec0e94c5657fcc0e694663ae1bb8e1a
Nidhi153 <npshahaa@gmail.com>
2023-05-06 19:02:17 +0800

    Trivial: Updated JavaDoc.

--- src/main/java/src/main/java/AppMain.java
+++ src/main/java/src/main/java/AppMain.java
@@ -5,8 +5,10 @@ import javafx.application.Application;
 
 /**
  * <h1>PPC1 Calculator</h1>
- * <p>
- *
+ * This document calculates the optimal production mix of Rose and Pinot Noir in three categories:
+ * 1. Annual Forecast maximizing Gross Profit
+ * 2. Weekly Review maximizing Revenue
+ * 3. Weekly Review maximizing Revenue after reserving Backorders
  *
  * @author  Nidhi Shah, Albertus Alexander and Calvin Wiogo
  * @version 1.0
